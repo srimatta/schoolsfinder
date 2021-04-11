45a6b50ef6602512af8fcbdfcb78ea04593e42aa
srinivas <srinivasu.matta@gmail.com>
2021-04-11 04:10:26 +0100

    added school type column.

--- src/main/scala/org/schools/SchoolDataService.scala
+++ src/main/scala/org/schools/SchoolDataService.scala
@@ -7,7 +7,8 @@ import org.apache.spark.sql.functions.col
 object SchoolDataService {
 
   //gender either Boys, Girls or Mixed
-  def getSchoolsData(spark: SparkSession, schoolsDataConfig: Config, gender: String = "Girls", ofstedPhase: String = "Secondary") = {
+  //"Secondary"
+  def getSchoolsData(spark: SparkSession, schoolsDataConfig: Config, gender: Array[String] =Array("Girls") , ofstedPhase: Array[String] = Array("Secondary")) = {
 
     val schoolsDataPath = schoolsDataConfig.getString("school_information")
     val schoolInspectionDataPath = schoolsDataConfig.getString("school_inspection")
@@ -16,14 +17,13 @@ object SchoolDataService {
     val schoolInspectionDF = spark.read.option("inferSchema", "true").option("header", true).csv(schoolInspectionDataPath)
     val schoolInformationDF = spark.read.option("inferSchema", "true").option("header", true).csv(schoolsDataPath)
 
-
     val schoolsDF = schoolInspectionDF.
       select("URN", "School name", "Ofsted phase", "Designated religious character",
         "Local authority", "Total number of pupils", "Postcode",
         "Overall effectiveness", "Parliamentary constituency").
-      join(schoolInformationDF.select("URN", "GENDER"), "URN").
-      filter(col("Ofsted phase") === ofstedPhase).
-      filter(col("GENDER") === gender).
+      join(schoolInformationDF.select("URN", "GENDER","SCHOOLTYPE"), "URN").
+      filter(col("Ofsted phase").isin(ofstedPhase: _*)).
+      filter(col("GENDER") isin(gender: _*)).
       withColumnRenamed("Designated religious character", "religious").
       withColumnRenamed("Total number of pupils", "Strength").
       withColumnRenamed("Parliamentary constituency", "constituency")
